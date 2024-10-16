54e7656ade04bf241a337aea34ff7d4c4e35d12f
Eddie <eyortega1@cps.edu>
2024-10-16 10:28:00 -0500

    I FUCKING DID IT, AGAIN.

--- Memes.py
+++ Memes.py
@@ -1,148 +1,150 @@
-memes_db: List[[Tuple[str,int,int,str]]] = [    
+from typing import List, Tuple
+
+memes_db: List[Tuple[str,int,int,str]] = [    
 (
-    "dat boi"
-    2016
-    1
-    "A Frog on a Unicycle, he says o shit waddup"
+    "dat boi",
+    2016,
+    1,
+    "A Frog on a Unicycle, he says o shit waddup",
 ),
 
 (
-    "harambe"
-    2016
-    2
-    "A cool Gorilla that got shot and killed in Ohio, he will always be remembered"
+    "harambe",
+    2016,
+    2,
+    "A cool Gorilla that got shot and killed in Ohio, he will always be remembered",
 ),
 
 (
-    "ohio"
-    2021
-    20
-    "It's a messed up place, the things that come out of there, are the things of the unspoken"
+    "ohio",
+    2021,
+    20,
+    "It's a messed up place, the things that come out of there, are the things of the unspoken",
 ),
 
 (
-    "rizz"
-    2021
-    17
-    "Unspoken Rizz"
+    "rizz",
+    2021,
+    17,
+    "Unspoken Rizz",
 ),
 
 (
-    "nine plus ten"
-    2013
-    19
-    "its twenty one"
+    "nine plus ten",
+    2013,
+    19,
+    "its twenty one",
 ),
 
 (
-    "big chungus"
-    2018
-    5
-    "It's a fat bugs bunny"
+    "big chungus",
+    2018,
+    5,
+    "It's a fat bugs bunny",
 ),
 
 (
-    "rick roll"
-    2008
-    8
-    "Mr Astley's surprise performance of his hit song"
+    "rick roll",
+    2008,
+    8,
+    "Mr Astley's surprise performance of his hit song",
 ),
 
 (
-    "among us"
-    2020
-    15
-    "scary imposter among us game by innersloth, the people that made henry stickmen"
+    "among us",
+    2020,
+    15,
+    "scary imposter among us game by innersloth, the people that made henry stickmen",
 ),
 
 (
-    "herobrine"
-    2010
-    18
-    "Scary steve man"
+    "herobrine",
+    2010,
+    18,
+    "Scary steve man",
 ),
 
 (
-    "in the thick of it"
-    2024
-    16
-    "song by the king of rap, KSI"
+    "in the thick of it",
+    2024,
+    16,
+    "song by the king of rap, KSI",
 ),
 
 (
-    "mlg"
-    2013
-    14
-    "snopp dogg, doritos, mtn dew, illumanti,"
+    "mlg",
+    2013,
+    14,
+    "snopp dogg, doritos, mtn dew, illumanti,",
 ),
 
 (
-    "sprite cranberry"
-    2017
-    13
-    "you want a sprite cranberry"
+    "sprite cranberry",
+    2017,
+    13,
+    "you want a sprite cranberry",
 ),
 
 (
-    "doge"
-    2014
-    12
-    "its a wow dog"
+    "doge",
+    2014,
+    12,
+    "its a wow dog",
 ),
 
 (
-    "dancing baby"
-    1996
-    11
-    "its a funny dancing baby"
+    "dancing baby",
+    1996,
+    11,
+    "its a funny dancing baby",
 ),
 
 (
-    "peanut butter jelly time"
-    2002
-    10
-    "its peanut butter jelly time!"
+    "peanut butter jelly time",
+    2002,
+    10,
+    "its peanut butter jelly time!",
 ),
 
 (
-    "baby yoda"
-    2019
-    9
-    "yoda but a baby"
+    "baby yoda",
+    2019,
+    9,
+    "yoda but a baby",
 ),
 
 (
-    "trollface"
-    2008    
-    8
-    "its a funny trollface"
+    "trollface",
+    2008,    
+    8,
+    "its a funny trollface",
 ),
 
 (
-    "nyan cat"
-    2013
-    7
-    "poptart cat in space"
+    "nyan cat",
+    2013,
+    7,
+    "poptart cat in space",
 ),
 
 (
-    "crank that"
-    2007
-    6
-    "Soulja boy, now watch me ohhhhhh"
+    "crank that",
+    2007,
+    6,
+    "Soulja boy, now watch me ohhhhhh",
 ),
 
 (
-    "this pic goes so hard"
-    2022
-    4
-    "feel free to screenshot"
-)
+    "this pic goes so hard",
+    2022,
+    4,
+    "feel free to screenshot",
+),
 
 (
-    "boneless pizza"
-    2016
-    3
-    "can I get that boneless pizza?"
+    "boneless pizza",
+    2016,
+    3,
+    "can I get that boneless pizza?",
 )
 ]
